b42dc4103f84d7aeefa4a58aa3f283616f964046
Juan Sebastian Peralta <jusepemu@MacBook-Pro-de-Juan.local>
2026-06-11 23:55:40 -0500

    style: update style to welcome page

--- src/routes/onboarding/welcome/+page.svelte
+++ src/routes/onboarding/welcome/+page.svelte
@@ -1,7 +1,17 @@
-<article class="flex flex-col gap-2">
-    <h2 class="text-3xl font-semibold">Toma el control de tu dinero.</h2>
-    <p class="text-gray-500">
-        Una forma simple y profesional de entender tus finanzas.
-    </p>
-    <button class="bg-primary text-secondary p-2 rounded">Comenzar</button>
+<script>
+    import Button from "$lib/components/ui/button/button.svelte";
+</script>
+
+<article class="flex flex-col gap-6 px-4 md:px-0 text-center">
+    <div class="flex flex-col gap-2">
+        <h2 class="text-4xl font-semibold">Toma el control de tu dinero.</h2>
+        <p class="text-lg text-gray-500">
+            Una forma simple y profesional de entender tus finanzas.
+        </p>
+    </div>
+
+    <Button
+        href="/onboarding/configure-profile"
+        class="bg-primary text-secondary">Comenzar</Button
+    >
 </article>
